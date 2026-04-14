215d2ffa7f7b3c1bc2732124e791d0d4f8437ffb
Mohamed Hasan <Modernkille3grow@gmail.com>
2026-04-14 15:43:52 +0300

    added classes into themeparks for css design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
 import Themeparks from "./components/Themeparks"
 import ParkDetails from "./components/ParkDetails"
 import Form from "./components/Form"
@@ -6,7 +6,6 @@ import Nav from "./components/Nav"
 import "./App.css"
 import { useState, useEffect } from "react"
 import axios from "axios"
-import { Link } from "react-router-dom"
 
 const App = () => {
   const [parks, setParks] = useState([])
@@ -14,8 +13,8 @@ const App = () => {
   useEffect(() => {
     const getParks = async () => {
       try {
-        let response = await axios.get(
-          "https://theme-park-stackathon-i3hh.onrender.com/parks"
+        const response = await axios.get(
+          "https://theme-park-stackathon-i3hh.onrender.com/themeparks"
         )
         setParks(response.data)
       } catch (err) {
@@ -39,19 +38,23 @@ const App = () => {
 
                 <div className="parks-grid">
                   {parks.map((park) => (
-                    <div className="park-card" key={park._id}>
-                      <Link to={`/themeparks/${park._id}`}>
+                    <Link
+                      to={`/themeparks/${park._id}`}
+                      className="card-link"
+                      key={park._id}
+                    >
+                      <div className="park-card">
                         <h3>{park.name}</h3>
-                      </Link>
 
-                      <img
-                        src={park.image}
-                        alt={park.name}
-                        className="park-image"
-                      />
+                        <img
+                          src={park.image}
+                          alt={park.name}
+                          className="park-image"
+                        />
 
-                      <p className="movie">🎬 {park.movie}</p>
-                    </div>
+                        <p className="movie">🎬 {park.movie}</p>
+                      </div>
+                    </Link>
                   ))}
                 </div>
               </>
