d4d3d9bf80ce2ea8161209e7ecea3210c74c1d61
Mohamed Hasan <Modernkille3grow@gmail.com>
2026-04-14 13:06:18 +0300

    finalized app.jsx created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Themeparks from "./components/Themeparks"
+import ParkDetails from "./components/ParkDetails"
+import Form from "./components/Form"
+import Nav from "./components/Nav"
 
 const App = () => {
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <Router>
+      <Nav />
+
+      <Routes>
+        <Route path="/themeparks" element={<Themeparks />} />
+        <Route path="/themeparks/new" element={<Form />} />
+        <Route path="/themeparks/:id" element={<ParkDetails />} />
+      </Routes>
+    </Router>
   )
 }
 
