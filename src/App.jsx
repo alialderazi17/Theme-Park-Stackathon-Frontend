0c413cc012d3dc15147613d9073dba1b3654a433
Ali Alderazi <ali.alderazi0@gmail.com>
2026-04-14 12:39:08 +0300

    moved components inside src

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css"
 import { Routes, Route } from "react-router-dom"
-import ParkDetails from "../components/ParkDetails"
-
+import ParkDetails from "./components/ParkDetails"
+import Form from "./components/Form"
 const App = () => {
   return (
     <>
       <h1>Hi</h1>
       <Routes>
         <Route path="/themeparks/:id" element={<ParkDetails />} />
+        <Route path="/themeparks/new" element={<Form />} />
       </Routes>
     </>
   )
